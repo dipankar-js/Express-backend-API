fb4040accb0ae561ad2e585cdad0b1092d08b312
Dipankar Barman <barman498@gmail.com>
2020-03-01 01:44:47 +0530

    bootcamp CRUD API done

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const colors = require('colors');
+const morgan = require('morgan');
+const connectDB = require('./config/db');
 
+// Load env vars
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Middleware
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Mount routes
+app.use('/api/v1/bootcamps', bootcamps);
+
+
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT, () => 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+const server = app.listen(PORT, () => 
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold)
     
     );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error : ${err.message}`);
+
+    // Close server
+    server.close(() => process.exit(1));
+})
