76ae4e46eee802f31b1848208c2397d16f961341
Dipankar498 <barman498@gmail.com>
2020-03-30 01:52:30 +0530

    get by radius added

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,22 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const 
-{   getBootcamps,
-    getBootcamp,
-    createBootcamp,
-    updateBootcamp,
-    deleteBootcamp
+const {
+	getBootcamps,
+	getBootcamp,
+	createBootcamp,
+	updateBootcamp,
+	deleteBootcamp,
+	getBootcampsInRadius
 } = require('../controller/bootcamps');
 
-router
-    .route('/')
-    .get(getBootcamps)
-    .post(createBootcamp)
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
-    .route('/:id')
-    .get(getBootcamp)
-    .put(updateBootcamp)
-    .delete(deleteBootcamp)
+	.route('/')
+	.get(getBootcamps)
+	.post(createBootcamp);
+
+router
+	.route('/:id')
+	.get(getBootcamp)
+	.put(updateBootcamp)
+	.delete(deleteBootcamp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require('morgan');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 // Load env vars
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
@@ -19,8 +19,8 @@ connectDB();
 const bootcamps = require('./routes/bootcamps');
 
 // Middleware
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
 }
 
 // Mount routes
@@ -30,15 +30,16 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-const server = app.listen(PORT, () => 
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold)
-    
-    );
+const server = app.listen(PORT, () =>
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.cyan.bold
+	)
+);
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
-    console.log(`Error : ${err.message}`);
+	console.log(`Error : ${err.message}`);
 
-    // Close server
-    server.close(() => process.exit(1));
-})
+	// Close server
+	server.close(() => process.exit(1));
+});
