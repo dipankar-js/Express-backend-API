b5ff84786c15bf8a96d02c2f41b03b3dc3588481
Dipankar498 <barman498@gmail.com>
2020-04-10 19:45:12 +0530

    update user and password done

--- controller/auth.js
+++ controller/auth.js
@@ -49,27 +49,6 @@ exports.login = asyncHandler(async (req, res, next) => {
 	sendTokenResponse(user, 200, res);
 });
 
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-	const token = user.getSignedJwtToken();
-
-	const options = {
-		expires: new Date(
-			Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-		),
-		httpOnly: true
-	};
-
-	if (process.env.NODE_ENV === 'production') {
-		options.secure = true;
-	}
-
-	res.status(statusCode).cookie('token', token, options).json({
-		success: true,
-		token
-	});
-};
-
 // @desc   Get current loggedIn user
 // @route  POST /api/v1/auth/me
 // @access Private
@@ -155,3 +134,61 @@ exports.resetPassword = asyncHandler(async (req, res, next) => {
 
 	sendTokenResponse(user, 200, res);
 });
+
+// @desc   Update user details
+// @route  PUT /api/v1/auth/updatedetails
+// @access Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+	const fieldsToUpdate = {
+		name: req.body.name,
+		email: req.body.email
+	};
+
+	const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({
+		success: true,
+		data: user
+	});
+});
+
+// @desc   Update password
+// @route  PUT /api/v1/auth/updatepasssword
+// @access Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+	const user = await User.findById(req.user.id).select('+password');
+
+	// Check current password
+	if (!(await user.matchedPassword(req.body.currentPassword))) {
+		return next(new ErrorResponse('Cuurent password is incorrect', 401));
+	}
+
+	user.password = req.body.newPassword;
+	await user.save();
+
+	sendTokenResponse(user, 200, res);
+});
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+	const token = user.getSignedJwtToken();
+
+	const options = {
+		expires: new Date(
+			Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+		),
+		httpOnly: true
+	};
+
+	if (process.env.NODE_ENV === 'production') {
+		options.secure = true;
+	}
+
+	res.status(statusCode).cookie('token', token, options).json({
+		success: true,
+		token
+	});
+};
